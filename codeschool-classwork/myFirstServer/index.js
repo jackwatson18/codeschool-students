6ca9905c39fe285d425c9df8a2e3328c26b7d22b
Jack Watson <jackhwatson18@gmail.com>
2023-06-16 12:55:01 -0600

    My first server

---
+++ codeschool-classwork/myFirstServer/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+
+const port = 8080;
+
+var myReminders = ["Make dinner", "Take out the trash", "Sleep"];
+
+app.get("/reminders", function(req, res) {
+    res.send(JSON.stringify(myReminders));
+})
+
+app.get("/reminders/:reminderId", function(req, res) {
+    var index = req.params.reminderId;
+    
+    if (index >= 0 && index < myReminders.length) {
+        // Valid index
+        res.send(JSON.stringify(myReminders[index]));
+    }
+
+    else {
+        // Invalid index
+        res.status(404).send("Reminder not found.");
+    }
+
+    // Idk what this is, its not an index though
+});
+
+app.post("/reminders", function (req, res) {
+    var new_reminder = req.body.reminder;
+
+    if (new_reminder) {
+        myReminders.push(new_reminder);
+        res.status(201).send("New reminder created.");
+    }
+    else {
+        res.status(422).send("Invalid request (no empty reminders please!)");
+    }
+    
+})
+
+app.put("/reminders/:reminderId", function(req, res) {
+    var index = req.params.reminderId;
+    
+    if (index >= 0 && index < myReminders.length) {
+        var updated_reminder = req.body.reminder;
+        if (updated_reminder) {
+            myReminders[index] = updated_reminder;
+            res.status(204).send("Reminder updated!");
+        }
+        else {
+            res.status(422).send("Invalid request (no empty reminder updates!)");
+        }
+    }
+
+    else {
+        res.status(404).send("Reminder not found.");
+    }
+
+})
+
+
+app.listen(port, function() {
+    console.log("Server started locally on port",port);
+})
